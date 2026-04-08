6d16bbabe5eac90d6345caf074d0075dae3d1551
matgizu <33757408+matgizu@users.noreply.github.com>
2026-04-08 17:37:28 -0500

    feat: update BASE_URL to caclutools.online in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,6 @@ import { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: "https://publisher-production-26af.up.railway.app/sitemap.xml",
+    sitemap: "https://caclutools.online/sitemap.xml",
   };
 }
